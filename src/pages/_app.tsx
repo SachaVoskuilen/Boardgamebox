4e3bb12639e01353a6a9f7060c2cc5acc17db2b0
Sacha Voskuilen <60003560+SachaVoskuilen@users.noreply.github.com>
2023-05-22 12:31:39 +0200

    feat(styling): styled-components and themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import "@/styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { DefaultTheme, GlobalStyle, SecondaryTheme } from "@/styles";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider theme={DefaultTheme}>
+      <SessionProvider session={session}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
   );
 };
 
